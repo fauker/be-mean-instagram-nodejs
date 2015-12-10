00f1485f7f9b6e57c0f09431da471a6df65e8a5f
Lucas Moreira <eskerlucas1@gmail.com>
2015-12-10 11:31:18 -0200

    Arquivos aula 01

---
+++ hello-http.js
@@ -0,0 +1,10 @@
+// hello-http.js
+var http = require('http');
+
+http.createServer(function(request, response){
+  response.writeHead(200, {'Content-Type': 'text/html'});
+  response.write('<h1>Be MEAN</h1>');
+  response.end();
+}).listen(3000, function(){
+  console.log('Servidor rodando em localhost:3000');
+});
